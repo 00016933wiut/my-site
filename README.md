5a9b23d76e1d813591a16130bb33d487ddab5fa4
00016933wiut <salijanov@students.wiut.uz>
2023-11-23 17:13:05 +0500

    uferfhb

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# my-site
+# Mehr va oqibar
+This site is for the volunteer organization "Mehr va oqibat". It has 5 pages: "Homepage", "Pets", "Donations", "Sponsors" and "About us", 1 CSS and 1 JavaScript files
+The purpose of my site is to bring attention to this organization and also increase awerenase of volunteer organizations in Uzbekistan.
+This website was created to fulfill Web Technology module’s requirements and does not represent an actual company or service 
--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     const carouselContainer = document.querySelector('.carousel-container');
     const slidesContainer = document.querySelector('.carousel-slides');
     const slides = document.querySelectorAll('.carousel-slide');
     const prevBtn = document.getElementById('prevBtn');
     const nextBtn = document.getElementById('nextBtn');
-
     let currentIndex = 0;
-    const itemsVisible = 3; 
+    const itemsVisible = 3;
 
     function showSlide(index) {
         slidesContainer.style.transform = `translateX(${(100 / itemsVisible) * -index}%)`;
@@ -25,11 +24,11 @@ document.addEventListener('DOMContentLoaded', function () {
     nextBtn.addEventListener('click', nextSlide);
     prevBtn.addEventListener('click', prevSlide);
 
-   
 });
 document.addEventListener('DOMContentLoaded', function() {
     const donate = document.querySelector('.donate');
     donate.addEventListener('click', calculate);
+
     function calculate() {
         const inputValue = document.getElementById('example').value;
         const number = parseFloat(inputValue);
@@ -41,19 +40,19 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 });
+
 function submitForm() {
     var f_name = document.getElementById('firstName');
     var l_name = document.getElementById('lastName');
     var user_email = document.getElementById('email');
     var message = document.getElementById('message');
-        if(f_name.value ==='' || l_name.value==='' || user_email.value==='' || message.value===''){
-            alert("fill all fields!");
-        }
-        else{
-            alert('Form submitted!');
-            f_name.value = '';
-            l_name.value = '';
-            user_email.value = '';
-            message.value = '';
-        }
+    if (f_name.value === '' || l_name.value === '' || user_email.value === '' || message.value === '') {
+        alert("fill all fields!");
+    } else {
+        alert('Form submitted!');
+        f_name.value = '';
+        l_name.value = '';
+        user_email.value = '';
+        message.value = '';
     }
+}
