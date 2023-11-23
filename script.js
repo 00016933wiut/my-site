11e1e57b57e04166065fd3dcdf4706321825ebcb
00016933wiut <salijanov@students.wiut.uz>
2023-11-23 12:40:12 +0500

    new change

--- script.js
+++ script.js
@@ -24,4 +24,36 @@ document.addEventListener('DOMContentLoaded', function () {
 
     nextBtn.addEventListener('click', nextSlide);
     prevBtn.addEventListener('click', prevSlide);
+
+   
 });
+document.addEventListener('DOMContentLoaded', function() {
+    const donate = document.querySelector('.donate');
+    donate.addEventListener('click', calculate);
+    function calculate() {
+        const inputValue = document.getElementById('example').value;
+        const number = parseFloat(inputValue);
+        if (!isNaN(number)) {
+            const result = number / 36000;
+            document.getElementById('number-of-pet-food').textContent = result.toFixed(2);
+        } else {
+            alert('Please enter a valid number');
+        }
+    }
+});
+function submitForm() {
+    var f_name = document.getElementById('firstName');
+    var l_name = document.getElementById('lastName');
+    var user_email = document.getElementById('email');
+    var message = document.getElementById('message');
+        if(f_name.value ==='' || l_name.value==='' || user_email.value==='' || message.value===''){
+            alert("fill all fields!");
+        }
+        else{
+            alert('Form submitted!');
+            f_name.value = '';
+            l_name.value = '';
+            user_email.value = '';
+            message.value = '';
+        }
+    }
